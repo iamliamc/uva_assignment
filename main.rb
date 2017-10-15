d814a516d199caea53a78b4e9248d2c38a27755e
Liam Considine <liam.considine@adtegrity.com>
2017-10-15 17:24:18 -0400

    Nokogiri parses it easily enough

--- main.rb
+++ main.rb
@@ -1,1 +1,12 @@
+require 'nokogiri'
+require 'open-uri'
 require_relative 'collection'
+
+source_url = "http://gss.uva.nl/binaries/content/assets/programmas/information-studies/txt-for-assignment-data-science.txt?3015083536432"
+
+doc = Nokogiri::HTML(open(source_url))
+
+doc.search('text').each_with_index do |link, index|
+  puts "TEXT #{index + 1}"
+  puts link.content
+end
